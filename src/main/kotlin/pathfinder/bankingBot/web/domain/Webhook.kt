1cc06bfa638496c0dbb19123df115fd0601bd815
TheMather1 <the.mather1@gmail.com>
2024-10-26 03:58:32 +0200

    Fix WebhookController

--- src/main/kotlin/pathfinder/bankingBot/web/domain/Webhook.kt
+++ src/main/kotlin/pathfinder/bankingBot/web/domain/Webhook.kt
@@ -1,10 +1,12 @@
 package pathfinder.bankingBot.web.domain
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonFormat.Shape.NUMBER
 
 data class Webhook(
     val version: Int,
+    @JsonAlias("application_id")
     val applicationId: String,
     @JsonFormat(shape = NUMBER)
     val type: Boolean,

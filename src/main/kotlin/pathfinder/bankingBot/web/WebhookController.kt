497bd2a2a6db0b3489660ad0a9d9a9c2bc872118
TheMather1 <the.mather1@gmail.com>
2024-10-26 03:11:15 +0200

    Implement tweetnacl.

--- src/main/kotlin/pathfinder/bankingBot/web/WebhookController.kt
+++ src/main/kotlin/pathfinder/bankingBot/web/WebhookController.kt
@@ -1,18 +1,30 @@
 package pathfinder.bankingBot.web
 
+
+import com.iwebpp.crypto.TweetNaclFast
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import pathfinder.bankingBot.web.domain.Webhook
 
 @Controller("/api/webhook")
 class WebhookController {
 
+    val publicKey = "".toByteArray()
+    val nacl = TweetNaclFast.Signature(ByteArray(0), publicKey)
+
     @PostMapping
     fun webhook(
+        @RequestHeader("X-Signature-Ed25519") signature: ByteArray,
+        @RequestHeader("X-Signature-Timestamp") timestamp: String,
         @RequestBody webhook: Webhook,
-    ): ResponseEntity<String> = if (webhook.type) ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
-    else ResponseEntity.noContent().build()
+        @RequestBody rawBody: String
+    ): ResponseEntity<String> = when {
+        !nacl.detached_verify("$timestamp$rawBody".toByteArray(), signature) -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        webhook.type -> ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+        else -> ResponseEntity.noContent().build()
+    }
 }

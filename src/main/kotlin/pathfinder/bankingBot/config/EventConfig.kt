9d1852a71d832db091f3053058548eb23c6fe1e1
TheMather1 <the.mather1@gmail.com>
2025-07-28 15:57:55 +0200

    Disable bean destruction of eventWaiter.

--- src/main/kotlin/pathfinder/bankingBot/config/EventConfig.kt
+++ src/main/kotlin/pathfinder/bankingBot/config/EventConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class EventConfig {
 
-    @Bean
+    @Bean(destroyMethod = "")
     fun eventWaiter() = EventWaiter()
 
 }
